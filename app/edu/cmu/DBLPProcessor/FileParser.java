2429384023189d509af7cca9d35fa150134bdc3f
Venkatesh Sriram <s.venkatesh.5789@gmail.com>
2013-11-23 14:09:05 -0800

    Removed pointless constructor from file parser file

--- app/edu/cmu/DBLPProcessor/FileParser.java
+++ app/edu/cmu/DBLPProcessor/FileParser.java
@@ -17,46 +17,43 @@ import org.xml.sax.SAXException;
 import edu.cmu.dataset.DBLPDataSource;
 import edu.cmu.dataset.DatasetInterface;
 
+@SuppressWarnings("unchecked")
 public class FileParser {
-	
-	public FileParser() {
-		super();
-	}
 
 	public void parseFile() throws SAXException, ParserConfigurationException, JAXBException, IOException{
 		HashMap<String,DBLPUser> dblp;
 		DatasetInterface dblpDataset = new DBLPDataSource();
 		dblp = dblpDataset.getDataset("Modified_dblp_example.xml");
-		
+
 		File file = new File("/Users/ShuaiWang/Desktop/Makedata.txt");		
 		FileWriter fw = new FileWriter(file.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
-		
+
 		Iterator<Map.Entry<String, DBLPUser>> i = dblp.entrySet().iterator(); 
 		while(i.hasNext()){
-		    String key = i.next().getKey();
-		    System.out.println(key);
-		    bw.write(key + ",");
-		    List<Publication> pubs = dblp.get(key).getPublication();
-		    for(Publication pname: pubs){
-		    	for(int j= 0; j< pname.getField().size();j++){
-		    		bw.write(pname.getField().get(j) + ",");
-		    	}
-		    	if(pname.getBooktitle()!=null){	
-		    		bw.write(pname.getBooktitle() + ",");
-	    		}
-		    	if(pname.getJournal()!=null){	
-		    		bw.write(pname.getJournal() + ",");
-	    		}
-		    	if(pname.getTitle()!=null){	
-		    		bw.write(pname.getTitle());
-	    		}
-		    }		    
-		    bw.write("\n");
+			String key = i.next().getKey();
+			System.out.println(key);
+			bw.write(key + ",");
+			List<Publication> pubs = dblp.get(key).getPublication();
+			for(Publication pname: pubs){
+				for(int j= 0; j< pname.getField().size();j++){
+					bw.write(pname.getField().get(j) + ",");
+				}
+				if(pname.getBooktitle()!=null){	
+					bw.write(pname.getBooktitle() + ",");
+				}
+				if(pname.getJournal()!=null){	
+					bw.write(pname.getJournal() + ",");
+				}
+				if(pname.getTitle()!=null){	
+					bw.write(pname.getTitle());
+				}
+			}		    
+			bw.write("\n");
 		}		
 		bw.close();		
 	}
-	
+
 	public static void main(String[] args) throws SAXException, ParserConfigurationException, JAXBException, IOException{
 		FileParser fp = new FileParser();
 		fp.parseFile();
