5f5ce945858f55c707fe76bd3cd6a950ce565988
“Shuai1989” <cmu.swang@gmail.com>
2013-11-30 22:35:48 -0800

    Finished trust calculation and distance in time scale function!

--- app/edu/cmu/ml/TextFileTrustAndCoauthorshipProcessor.java
+++ app/edu/cmu/ml/TextFileTrustAndCoauthorshipProcessor.java
@@ -3,6 +3,7 @@ package edu.cmu.ml;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import edu.cmu.database.DbOperations;
 import edu.cmu.trustprocessor.TimeScale;
@@ -12,23 +13,34 @@ public class TextFileTrustAndCoauthorshipProcessor {
 	private String fileName;
 	private TextFilePublicationParser publications;
 	private TimeScale timescale;
-	private DbOperations db;
+	private static DbOperations db;
 	
-	private double alphaArticle =TrustModelWeights.alphaArticle;
-	private double alphaBook = TrustModelWeights.alphaBook;
-	private double alphaInCollection = TrustModelWeights.alphaInCollection;
-	private double alphaInProceeding = TrustModelWeights.alphaInProceeding;
-	private double alphaMasterThesis=TrustModelWeights.alphaMasterThesis;
-	private double alphaPhdThesis = TrustModelWeights.alphaPhdThesis;
-	private double alphaProceeding = TrustModelWeights.alphaProceeding;
-	private double alphaWWW=TrustModelWeights.alphaWWW;
-
+	private TrustModelWeights trustModelWeights;
+	private double alphaArticle;
+	private double alphaBook;
+	private double alphaInCollection;
+	private double alphaInProceeding;
+	private double alphaMasterThesis;
+	private double alphaPhdThesis;
+	private double alphaProceeding;
+	private double alphaWWW;
+	
 	public TextFileTrustAndCoauthorshipProcessor(String fileName) throws IOException, ClassNotFoundException, SQLException {
 		super();
 		this.fileName = fileName;
 		this.publications = new TextFilePublicationParser(fileName);
+		trustModelWeights = new TrustModelWeights();
 		this.timescale = new TimeScale();
 		this.db = new DbOperations();
+		
+		alphaArticle =trustModelWeights.alphaArticle;
+		alphaBook = trustModelWeights.alphaBook;
+		alphaInCollection = trustModelWeights.alphaInCollection;
+		alphaInProceeding = trustModelWeights.alphaInProceeding;
+		alphaMasterThesis=trustModelWeights.alphaMasterThesis;
+		alphaPhdThesis = trustModelWeights.alphaPhdThesis;
+		alphaProceeding = trustModelWeights.alphaProceeding;
+		alphaWWW= trustModelWeights.alphaWWW;
 	}
 	
 	/**
@@ -41,16 +53,23 @@ public class TextFileTrustAndCoauthorshipProcessor {
 	 * @throws SQLException 
 	 */
 	public int getCoauthorDistanceBeforeYear (String authorName1, String authorName2, int year) throws SQLException {
-		int distance = 0;
-		//TODO: Shuai's method to get the coauthors and coauthors of coauthors goes here
-		//TODO: need to parse the return of the method in order to find out the 
-		ResultSet result = db.callDatabaseQuery("SELECT DISTINCT Coauthor FROM coauthor.Coauthors WHERE Author = '" + authorName1 + "'");
-		
+		ResultSet result = db.callDatabaseQuery("SELECT DISTINCT Coauthor FROM coauthor.Coauthors WHERE Author = '" + authorName1 + "' AND Year<" + year + ";");
+		ArrayList<String> coauthors = new ArrayList<String>();
 		while(result.next()) {
+			coauthors.add(result.getString("Coauthor"));
 			if(result.getString("Coauthor").equalsIgnoreCase(authorName2))
 				return 1;
 		}
-		return distance;	
+		
+		for(String authorName: coauthors) {
+			result = db.callDatabaseQuery("SELECT DISTINCT Coauthor FROM coauthor.Coauthors WHERE Author = '" + authorName + "' AND Year<" + year + ";");
+			while(result.next()) {
+				if(result.getString("Coauthor").equalsIgnoreCase(authorName2))
+					return 2;
+			}
+		}
+		
+		return 999;	
 	}
 	
 	/**
@@ -59,8 +78,9 @@ public class TextFileTrustAndCoauthorshipProcessor {
 	 * @param name
 	 * @param year
 	 * @return
+	 * @throws SQLException 
 	 */
-	public Double calculateTrustBeforeYear(String name, int year) {
+	public Double calculateTrustBeforeYear(String name, int year) throws SQLException {
 		Double publicationTrust = 0.0;
 		Double coauthorshipTrust = 0.0;
 		
@@ -70,7 +90,7 @@ public class TextFileTrustAndCoauthorshipProcessor {
 		return publicationTrust + coauthorshipTrust;
 	}
 
-	private Double calculateCoauthorshipTrust(String authorName, int year) {
+	private Double calculateCoauthorshipTrust(String authorName, int year) throws SQLException {
 		double coauthorshipTrust = 0.0;
 		
 		int numberOfRecentCoauthorships = getNumberOfCoauthorsInTimeRange(authorName, year - timescale.getRecentYears(), year);
@@ -83,7 +103,7 @@ public class TextFileTrustAndCoauthorshipProcessor {
 		return coauthorshipTrust;
 	}
 
-	private Double calculatePublicationTrust(String authorName, int year) {
+	private Double calculatePublicationTrust(String authorName, int year) throws SQLException {
 		Double publicationTrust = 0.0;
 		
 		int numOfArticleRecent = getCountPublicationsBetweenYears(authorName, "article", year - timescale.getRecentYears(), year);
@@ -118,14 +138,14 @@ public class TextFileTrustAndCoauthorshipProcessor {
 		int numOfWWWIntermediate = getCountPublicationsBetweenYears(authorName, "www", year - timescale.getIntermediateYears(), timescale.getRecentYears());
 		int numOfWWWOld = getCountPublicationsBetweenYears(authorName, "www", 1800 , year - timescale.getIntermediateYears());
 		
-		int numberOfCitationsA = getTotalCitationsOfAuthorByType(authorName, "article");
-		int numberOfCitationsB = getTotalCitationsOfAuthorByType(authorName, "book");
-		int numberOfCitationsIC = getTotalCitationsOfAuthorByType(authorName, "incollections");
-		int numberOfCitationsIP = getTotalCitationsOfAuthorByType(authorName, "inproceedings");
-		int numberOfCitationsM = getTotalCitationsOfAuthorByType(authorName, "mastersthesis");
-		int numberOfCitationsPH = getTotalCitationsOfAuthorByType(authorName, "phdthesis");
-		int numberOfCitationsP = getTotalCitationsOfAuthorByType(authorName, "proceedings");
-		int numberOfCitationsW = getTotalCitationsOfAuthorByType(authorName, "www");
+		int numberOfCitationsA = getTotalCitationsOfAuthorByType(authorName, "article", year);
+		int numberOfCitationsB = getTotalCitationsOfAuthorByType(authorName, "book", year);
+		int numberOfCitationsIC = getTotalCitationsOfAuthorByType(authorName, "incollections", year);
+		int numberOfCitationsIP = getTotalCitationsOfAuthorByType(authorName, "inproceedings", year);
+		int numberOfCitationsM = getTotalCitationsOfAuthorByType(authorName, "mastersthesis", year);
+		int numberOfCitationsPH = getTotalCitationsOfAuthorByType(authorName, "phdthesis", year);
+		int numberOfCitationsP = getTotalCitationsOfAuthorByType(authorName, "proceedings", year);
+		int numberOfCitationsW = getTotalCitationsOfAuthorByType(authorName, "www", year);
 		
 		publicationTrust = alphaArticle * (timescale.gettRecent() * numOfArticleRecent
 										 + timescale.gettIntermediate() * numOfArticleIntermediate
@@ -163,18 +183,49 @@ public class TextFileTrustAndCoauthorshipProcessor {
 		return publicationTrust;
 	}
 
-	private int getTotalCitationsOfAuthorByType(String authorName, String type) {
-		// TODO Shuai's db method goes here.
-		return 0;
+	public int getTotalCitationsOfAuthorByType(String authorName, String type, int year) throws SQLException {
+		int totalCitations = 0;
+		ArrayList<String> citations = new ArrayList<String>();
+		ResultSet result1 = db.callDatabaseQuery("SELECT PublicationKey FROM coauthor.Publications WHERE Author_Title LIKE '" + authorName + "%' AND Year<" + year + " AND Type = '" + type + "';");
+		ResultSet result2;
+		while(result1.next()) {
+			citations.add(result1.getString("PublicationKey"));
+		}
+		
+		for(String key : citations) {
+			result2 = db.callDatabaseQuery("SELECT COUNT(Author_Title) FROM coauthor.Publications WHERE Citations LIKE '%" + key + "%' AND Year<" + year + ";");
+			while(result2.next())
+				totalCitations+=result2.getInt("COUNT(Author_Title)");
+		};
+		
+		return totalCitations;
 	}
 
-	private int getCountPublicationsBetweenYears(String authorName, String type, int startingYear, int endingYear) {
-		// TODO Shuai's db method goes here.
-		return 0;
+	public int getCountPublicationsBetweenYears(String authorName, String type, int startingYear, int endingYear) throws SQLException {
+		ResultSet result = db.callDatabaseQuery("SELECT COUNT(Author_Title) FROM coauthor.Publications WHERE Author_Title LIKE '" + authorName + "%' AND Year>" + startingYear + " AND Year<" + endingYear +  " AND Type = '" + type + "';");
+		while(result.next())
+			return result.getInt("COUNT(Author_Title)");
+		return -100;
 	}
 	
-	private int getNumberOfCoauthorsInTimeRange(String authorName, int startingYear, int endingYear) {
-		// TODO Shuai's db method goes here.
-		return 0;
+	public int getNumberOfCoauthorsInTimeRange(String authorName, int startingYear, int endingYear) throws SQLException {
+		ResultSet result = db.callDatabaseQuery("SELECT COUNT(DISTINCT Coauthor) FROM coauthor.Coauthors WHERE Author = '" + authorName + "' AND Year >=" + startingYear + " AND Year<=" + endingYear + ";");
+		while(result.next())
+			return result.getInt("COUNT(DISTINCT Coauthor)");
+		return -100;
+	}
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+		TextFileTrustAndCoauthorshipProcessor blah = new TextFileTrustAndCoauthorshipProcessor("ds.txt");
+		
+//		ResultSet result = db.callDatabaseQuery("SELECT DISTINCT Author FROM coauthor.Coauthors ;");
+//		ArrayList<String> blahasdf = new ArrayList<String>();
+//		while(result.next())
+//			blahasdf.add(result.getString("Author"));
+//		
+//		System.out.println(blahasdf.size());
+//		for(String author: blahasdf)
+//			System.out.println(blah.calculateTrustBeforeYear(author, 2012));
+		System.out.println(blah.getCoauthorDistanceBeforeYear("Hassan Artail", "Ghada Derbas", 2012));
 	}
 }
